67c8dfbc3f65f43af7ba5f3478c8c29980565bb6
ritushreedas2002 <ritushreedas20027@gmail.com>
2024-11-11 20:13:27 +0530

    pages connection

--- src/app/Navbar.tsx
+++ src/app/Navbar.tsx
@@ -1,10 +1,12 @@
+import Link from "next/link";
+
 const Navbar = () => {
     return (
       <div className="w-full p-5 flex justify-between items-center bg-black text-white">
         {/* Logo */}
         <div className="text-2xl font-bold">XYZ</div>
   
-        {/* Desktop Links */}
+        
         <div className="hidden md:flex space-x-4">
           <a href="#" className="hover:underline">
             Ride
@@ -20,14 +22,10 @@ const Navbar = () => {
           </a>
         </div>
   
-        {/* Log in and Sign up (Visible on all screens) */}
         <div className="space-x-4 flex items-center">
-          <a href="#" className="text-sm hover:underline">
+          <Link href="/Pages/Login" className="text-sm hover:underline">
             Log in
-          </a>
-          <button className="bg-white text-black px-4 py-2 rounded-full">
-            Sign up
-          </button>
+          </Link>
         </div>
       </div>
     );
